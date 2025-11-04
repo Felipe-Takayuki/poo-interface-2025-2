80870b09d4a27b7237b24c71a5bd8c0bb3d66268
Felipe <105937609+Felipe-Takayuki@users.noreply.github.com>
2025-11-04 01:42:51 +0000

    03-11-2025 - interfaces e classes

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,7 +2,19 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        NotificacaoSms notifSMS = new NotificacaoSms("Felipe", "14999999999", "Mensagem de Teste", 1); 
+        notifSMS.exibirInformacoesRemetente();
+        notifSMS.enviar();
+        notifSMS.registrarLog(); 
+        notifSMS.definirPrioridade(8);
+        notifSMS.obterNivelPrioridade(); 
+        notifSMS.definirPrioridade(0);
+        Notificacao notif = new NotificacaoSms("Fulano", "14131312412412", "Testando", 3);
+        notif.exibirInformacoesRemetente();
+        notif.enviar();
+        Priorizavel objPriorizavel = new NotificacaoSms("Ciclano", "148109481094812904", "Olá", 8);
+        
+        
     }
 }
 
